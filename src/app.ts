8b313fc57fe480523e1c1099876bc49e4f63f194
NeoticoZ <heriksaraujodias@gmail.com>
2022-05-27 12:11:54 -0300

    adjusted to deploy

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import "reflect-metadata";
 
 const app = express();
 
+const port = process.env.PORT || 3333;
+
 app.use(cors());
 app.use(express.json());
 app.use(router);
@@ -24,4 +26,4 @@ app.use(
   }
 );
 
-app.listen(3333, () => console.log("Server started on port 3333"));
+app.listen(port, () => console.log("Server started on port 3333"));
